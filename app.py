0fa5747317de03f8378979e150d73334f13062e5
Gabriel Rocha <gabriel.rocha.gbr@gmail.com>
2017-03-11 20:41:18 -0300

    App port

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ def page_not_found(e):
     return render_template('status_code/500.html'), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run("0.0.0.0")
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
